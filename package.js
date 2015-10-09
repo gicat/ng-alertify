3dcbe3a5e1b59a2e5dcef8319686e16877498e70
gicat <gpcatalano@gmail.com>
2015-10-09 14:33:30 +0200

    Added configuration file for meteorjs package

---
+++ package.js
@@ -0,0 +1,20 @@
+// package metadata file for Meteor.js
+var packageName = 'ng-alertify';
+var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
+var version = '0.9.0';
+var summary = 'AngularJS wrapper around alertify.js';
+var gitLink = 'https://github.com/kensho/ng-alertify';
+var documentationFile = 'README.md';
+
+// Meta-data
+Package.describe({
+  name: packageName,
+  version: version,
+  summary: summary,
+  git: gitLink,
+  documentation: documentationFile
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.2']); // Meteor versions
+});
